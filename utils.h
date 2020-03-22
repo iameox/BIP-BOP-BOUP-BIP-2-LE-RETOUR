31ac94553ed9f5a22f49d9eb53826e4b128706df
dautreym <51315556+dautreym@users.noreply.github.com>
2020-03-22 19:03:31 +0100

    Début de parseur fonctionnel - à ré-arranger

---
+++ utils.h
@@ -0,0 +1,27 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int compter_caractere(char *s, char c);
+
+int compter_nombre_de_lignes(char *s);
+
+int taille_ligne(char *s);
+
+void recopier(char *src, char *dest, int nb_recopies);
+
+char* trouver_ligne_n(char *s, int n, int taille_ligne_n);
+
+int supprimer_espace(char *s);
+
+int compter_mots(char *s);
+
+int taille_mot(char *mot);
+
+int nombre_espaces_apres_chaine(char *ligne);
+
+char* trouver_mot_n(char *s, int n, int taille_mot_n);
+
+#endif
