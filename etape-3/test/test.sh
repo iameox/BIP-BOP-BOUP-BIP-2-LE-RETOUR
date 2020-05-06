20f71e4801e3c5a4bf408be53fffbeb00db40544
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2020-05-06 17:19:42 +0200

    ajout de plus de tests
    différentes méthodes dont des méthodes inexistantes, puis des hosts différents

--- etape-3/test/test.sh
+++ etape-3/test/test.sh
@@ -3,5 +3,10 @@
 
 printf 'GET / HTTP/1.0\r\n\r\n' | netcat 127.0.0.1 8080
 #printf 'GET / HTTP/1.1\r\n\r\n' | netcat 127.0.0.1 8080
-printf 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n' | netcat 127.0.0.1 8080
-printf 'GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\n\r\n' | netcat 127.0.0.1 8080
+printf 'HEAD / HTTP/1.0\r\n\r\n' | netcat 127.0.0.1 8080
+printf 'POST / HTTP/1.0\r\n\r\n' | netcat 127.0.0.1 8080
+printf 'SUPERMETHODEDU47QUIEXISTEPAS / HTTP/1.0\r\n\r\n' | netcat 127.0.0.1 8080
+#printf 'GET / HTTP/1.1\r\nHost: localhost\r\n\r\n' | netcat 127.0.0.1 8080
+printf 'GET / HTTP/1.1\r\nHost: www.example.com\r\n\r\n' | netcat 127.0.0.1 8080
+#printf 'GET / HTTP/1.1\r\nHost: www.supersitedu47.triz\r\n\r\n' | netcat 127.0.0.1 8080
+#printf 'GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:45.0) Gecko/20100101 Firefox/45.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\n\r\n' | netcat 127.0.0.1 8080
