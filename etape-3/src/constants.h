ce783b8b0dfeb8891faa193d718e2bb53f682a40
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2020-05-06 11:26:20 +0200

    Premier jeu de tests, + qq fonctions

--- etape-3/src/constants.h
+++ etape-3/src/constants.h
@@ -1,2 +1,10 @@
 #define true 1
 #define false 0
+
+#define min(a,b) a<b?a:b
+
+//A changer probablement en /var/www/html/
+#define ROOT "./"
+
+#define KNOWN_METHODS {"GET", "HEAD", "POST"}
+#define METHODS_NUMBER 3
---
+++ etape-3/src/fonctions_lucas.c
@@ -0,0 +1,57 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "constants.h"
+
+int validMethod(char * method, int len) {
+	int valid = false;
+
+	int i = 0;
+	while(!valid && i < METHODS_NUMBER) {
+		if(!strncmp(method, KNOWN_METHODS[i], min(len, strlen(KNOWN_METHODS[i])))) {
+			printf("la méthode c'est un %s\n", KNOWN_METHODS[i]);
+			valid = true;
+		}
+	}
+
+	if(!valid) {
+		printf("pas de méthode trouvée\n");
+	}
+
+	return valid;
+}
+
+/*
+* Indique si une ressource décrite par le couple (request_target, host) est disponible.
+* On considère que la request_target est de type origin-form
+* Retourne 1 si la ressource est disponible, 0 sinon
+*/
+int isAvailable(char * request_target, int request_target_length, char * host, int host_size) {
+	int available = true;
+	int i = 0;
+
+	char *name = malloc((strlen(ROOT) + request_target_length + host_size)*sizeof(char));
+
+	//On recopie le début de l'arborescence (propre au serveur)
+	for(i = 0 ; i < strlen(ROOT) ; i++) name[i] = ROOT[i];
+	//host
+	for( ; i < request_target_length ; i++) name[i] = request_target[i];
+	//request-target
+	for( ; i < host_size ; i++) name[i] = host[i];
+
+	//On considère que c'est du origin-form
+	FILE * ressource = fopen(name, "r");
+	
+	if(ressource == NULL) {
+		available = false;
+	}
+
+	free(name);
+	return available;
+}
+
+
+int main() {
+	return 1;
+}
