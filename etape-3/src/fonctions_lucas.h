a78cf2faa27cf4888a4aa479107d1833f6485838
Rigos07 <51322232+Rigos07@users.noreply.github.com>
2020-05-19 13:38:50 +0200

    Nouveaux sites de tests

--- etape-3/src/fonctions_lucas.h
+++ etape-3/src/fonctions_lucas.h
@@ -1,8 +1,8 @@
 // Gestion du multi-site et de l'arborescence
 #define ROOT_PATH "./../www/"
-#define KNOWN_HOSTS_LIST {"www.example.com:8080", "www.supersitedu47.wow:8080", "localhost:8080"}
-#define KNOWN_HOSTS_PATHS {"example", "supersitedu47", "example"}
-#define KNOWN_HOSTS_COUNT 3
+#define KNOWN_HOSTS_LIST {"www.example.com:8080", "www.supersitedu47.wow:8080", "www.fake.com:8080", "www.toto.com:8080"}
+#define KNOWN_HOSTS_PATHS {"example", "supersitedu47", "fake", "toto"}
+#define KNOWN_HOSTS_COUNT 4
 
 int validMethod(string *method);
 char *isAvailable(string *request_target, string *host, int *len);
