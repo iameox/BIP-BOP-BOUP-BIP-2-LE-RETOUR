301683f10fe4de8efbbbcd51fc1731c40e7452c8
dautreym <51315556+dautreym@users.noreply.github.com>
2020-05-22 19:43:00 +0200

    une disjonction EXHAUSTIVE des 66 extensions courantes

--- etape-3/src/fonctions_marin.h
+++ etape-3/src/fonctions_marin.h
@@ -4,4 +4,8 @@ int are_unique_headers(void *root);
 //int is_http_version_ok(void *root);
 int is_http_version_ok(string *http_version, string *host_header);
 
-string *get_mime_type(char *filename);
+
+string* get_mime_type(string *filename);
+
+
+string* get_mime_type_with_magic(char *filename);
